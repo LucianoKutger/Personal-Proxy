5ac69a5d55ab5d9bf9a18ca1e384e277fe6ece50
Luciano Kutger <Luciano.kutger@web.de>
2025-06-04 16:34:18 +0200

    reversed file input

--- proxy-server/services/openai.js
+++ proxy-server/services/openai.js
@@ -7,8 +7,6 @@ require("dotenv").config();
 //env vars
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const SYSTEM_INSTRUCTIONS = process.env.SYSTEM_INSTRUCTIONS;
-const DIN24495 = process.env.DIN24495
-const DIN8581 = process.env.DIN8581
 
 const client = new openai.OpenAI({ apiKey: OPENAI_API_KEY });
 
@@ -16,14 +14,14 @@ const client = new openai.OpenAI({ apiKey: OPENAI_API_KEY });
 
 async function translateParagraph(paragraph) {
   try {
-    const response = await client.responses.create({
-      model: "gpt-4.1",
-      input: [
+    const response = await client.chat.completions.create({
+      model: "gpt-4.1-mini",
+      messages: [
         {
           role: "system",
           content: [
             {
-              type: "input_text",
+              type: "text",
               text: SYSTEM_INSTRUCTIONS,
             },
           ],
@@ -32,23 +30,15 @@ async function translateParagraph(paragraph) {
           role: "user",
           content: [
             {
-              type: "input_file",
-              file_id: DIN24495
-            },
-            {
-              type: "input_file",
-              file_id: DIN8581
-            },
-            {
-              type: "input_text",
-              text: paragraph
+              type: "text",
+              text: paragraph,
             },
           ],
         },
       ],
     });
 
-    return response.output_text
+    return response.choices[0].message.content;
 
   } catch (error) {
     console.error('Fehler: ', error)
