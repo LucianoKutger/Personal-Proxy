5c01685b8d26391f1942da1548a49f6b2478644e
Luciano Kutger <Luciano.kutger@web.de>
2025-08-27 15:04:00 +0200

    add env var in code

--- proxy-server/services/easyReader/openai.js
+++ proxy-server/services/easyReader/openai.js
@@ -7,7 +7,8 @@ require("dotenv").config();
 //env vars
 const OPENAI_API_KEY = process.env.OPENAI_KEY_PROJECT;
 //const OPENAI_API_KEY_DEV = process.env.OPENAI_API_KEY_EASYREADER;
-const SYSTEM_INSTRUCTIONS = process.env.SYSTEM_INSTRUCTIONS_EASYREADER;
+const SYSTEM_PROMPT_EASYREADER = process.env.SYSTEM_PROMPT_EASYREADER;
+const USER_PROMPT_EASYREADER = process.env.USER_PROMPT_EASYREADER;
 const SYSTEM_INSTRUCTIONS_PLAIN_GERMAN = process.env.SYSTEM_INSTRUCTIONS_PLAIN_GERMAN_EASYREADER;
 
 const client = new openai.OpenAI({ apiKey: OPENAI_API_KEY });
@@ -24,7 +25,7 @@ async function translateParagraph(paragraph) {
           content: [
             {
               type: "text",
-              text: SYSTEM_PROMPT_EASYREADER,
+              text: SYSTEM_PROMPT_EASYREADER
             }
           ]
         },
@@ -33,7 +34,7 @@ async function translateParagraph(paragraph) {
           content: [
             {
               type: "text",
-              text: USER_PROMPT_EASYREADER + "\n" + paragraph,
+              text: USER_PROMPT_EASYREADER + "\n" + paragraph
               //text: "Übersetze folgenden HTML tag in einfache Sprache nach der DIN 8581-1 und DIN ISO 24495-1, halte dich an folgende Vorgaben: \n" + SYSTEM_INSTRUCTIONS + "\n achte darauf das alle Vorgaben eingehalten wurden wie z.B. die Länge des Satzes etc. prüfe vor der Finalen übergabe der Übersetzung akribisch ob alle Vorgaben erfüllt wurden und ändere gegebenenfalls noch etwas ab sodass diese eingehalten werden. Achte darauf das du mir ausschließlich den Tag gibst mit Überstezten Inhalt. Übersetze die inhalte des HTML Tags ausschöießlich auf Deutsch, behalte alle Inline tags bei, keine Kommentare, keine zusatz bemerkungen, einfach den überstezten HTML tag in den Chat. \n hier der zu übersetzende Tag:\n " + paragraph,
             },
           ],
